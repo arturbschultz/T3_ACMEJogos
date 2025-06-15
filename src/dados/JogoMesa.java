3c85d7bae7b1afbe1aced8a83521530e72fe1a16
Arthur Kasprzak <kasprzakramos@icloud.com>
2025-06-15 20:15:53 -0300

    feat: Implementa sistema de catálogo e cálculo de aluguel

--- src/dados/JogoMesa.java
+++ src/dados/JogoMesa.java
@@ -4,6 +4,20 @@ public class JogoMesa extends Jogo {
     private TipoMesa tipo;
     private int numeroPecas;
 
+    public JogoMesa(int codigo, String nome, double valorBase, TipoMesa tipo, int numeroPecas) {
+        super(codigo, nome, valorBase);
+        this.tipo = tipo;
+        this.numeroPecas = numeroPecas;
+    }
+
+    public TipoMesa getTipo() {
+        return tipo;
+    }
+
+    public int getNumeroPecas() {
+        return numeroPecas;
+    }
+
     @Override
     public double calculaAluguel() {
         return 0;
